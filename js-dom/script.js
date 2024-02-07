036e8517f6e0338aa48f0209b18613945632830e
Cocar Ciprian <Cocar Ciprian>
2024-02-07 18:18:41 +0200

    junior

--- js-dom/script.js
+++ js-dom/script.js
@@ -11,9 +11,18 @@ sendFilesButton.addEventListener("click", () => {
 const shoppingList = document.getElementsByClassName("list-item");
 const shoppingDoneButton = document.getElementById("shopping-done");
 
+const shoppingListDone = document.getElementById("shopping-list-done");
+
 for (let item of shoppingList) {
   item.addEventListener("click", () => {
+    const shoppingListDone = document.createElement("li");
+    shoppingListDone.innerText = item.innerText;
+
+    // shoppingListDone.appendChild(shoppingListDoneItem);
+
     item.innerText = item.innerText + " (Cumparat)";
+
+    //item.removeEventListener("click");
   });
 }
 
@@ -22,3 +31,50 @@ shoppingDoneButton.addEventListener("click", () => {
     item.innerText = item.innerText + "(Cumparat)";
   }
 });
+
+const allLiItems = document.getElementsByTagName("li");
+console.log(allLiItems);
+
+const titleWithQuerySelector = document.querySelectorAll(".shopping-list .list-item:nth-of-type(3)");
+console.log(titleWithQuerySelector);
+
+const backgroundColors = ["red", "green", "blue", "black"];
+
+const rectangle = document.querySelector(".rectangle");
+rectangle.addEventListener("click", () => {
+  rectangle.style.backgroundColors = backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
+  rectangle.classList.toggle("rectangle-style-1");
+});
+const titleElement = document.createElement("h1");
+rectangle.innerHTML = "<h1>Acesta este un titlu</h1>";
+titleElement.classList.add("title");
+
+rectangle.appendChild(titleElement);
+
+// let listElement = document.createElement("ul");
+// let listItemElement = document.createElement("li");
+// listItemElement.innerText = "List item 1";
+
+// let listItemElement = document.createElement("li");
+// listItemElement.innerText = "List item 2";
+
+// let listItemElement = document.createElement("li");
+// listItemElement.innerText = "List item 3";
+
+// let listItemElement = document.createElement("li");
+// listItemElement.innerText = "List item 4";
+
+// listElement.appendChild(listItemElement);
+// listElement.appendChild(listItemElement);
+// listElement.appendChild(listItemElement);
+// listElement.appendChild(listItemElement);
+
+// listElement.append(listItem1Element, listItem2Element, listItem3Element, listItem4Element);
+
+const link = document.createElement("a");
+link.innerText = "Link to Google";
+
+link.setAttribute("href", "https://google.ro");
+link.setAttribute("target", "_blank");
+
+rectangle.appendChild(link);
