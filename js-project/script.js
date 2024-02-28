611e82fd552441236bfa711f10cf9c97d7b4a11c
Cocar Ciprian <Cocar Ciprian>
2024-02-28 18:05:15 +0200

    TOt

--- js-project/script.js
+++ js-project/script.js
@@ -29,6 +29,7 @@ addColumnButton.addEventListener("click", () => {
 
   columns.push({ name: newColumnName, tasks: [] });
   saveColumns();
+  refreshBoard();
 });
 
 function getDefaultColumns() {
@@ -49,13 +50,66 @@ function showColumns() {
     const columnElement = document.createElement("div");
     columnElement.classList.add("column");
 
+    const columnTitleContainer = document.createElement("div");
+    columnTitleContainer.classList.add("title-container");
+
     const columnTitleElement = document.createElement("h3");
+    columnTitleElement.classList.add("title");
     columnTitleElement.textContent = column.name;
 
-    columnElement.appendChild(columnTitleElement);
+    const columnTitleDeleteButton = document.createElement("button");
+    columnTitleDeleteButton.classList.add("delete");
+    columnTitleDeleteButton.addEventListener("click", () => {
+      deleteColumn(column.name);
+    });
+    columnTitleDeleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
+    columnTitleContainer.appendChild(columnTitleElement);
+    columnTitleContainer.appendChild(columnTitleDeleteButton);
+
+    const taskConntainerElement = document.createElement("div");
+    taskConntainerElement.classList.add("tasks");
+
+    const createTaskInputLabel = document.createElement("p");
+    createTaskInputLabel.classList.add("label");
+    createTaskInputLabel.textContent = "Adauga un nou task...";
+    createTaskInputLabel.style.display = "none";
+
+    const createTaskInput = document.createElement("input");
+    createTaskInput.classList.add("create-task");
+    createTaskInput.setAttribute("type", "text");
+    createTaskInput.style.display = "none";
+
+    columnElement.appendChild(columnTitleContainer);
+    columnElement.appendChild(taskConntainerElement);
+    columnElement.appendChild(createTaskInputLabel);
+    columnElement.appendChild(createTaskInput);
+
+    taskConntainerElement.addEventListener("click", () => {
+      createTaskInputLabel.style.display = "block";
+      createTaskInput.style.display = "block";
+    });
 
     boardElement.appendChild(columnElement);
   });
+}
+
+function clearBoard() {
+  boardElement.innerHTML = "";
+}
+
+function refreshBoard() {
+  clearBoard();
+  showColumns();
+}
+
+function deleteColumn(columnName) {
+  columns = columns.filter((column) => {
+    return column.name !== columnName;
+  });
+  saveColumns();
+
+  refreshBoard();
 }
 
 getDefaultBoardName();
